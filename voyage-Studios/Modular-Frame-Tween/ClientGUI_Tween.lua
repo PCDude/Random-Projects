2dae93b22429b19b3ff68c1d87768b4192c0db77
PCDude <pcdudebusiness@outlook.com>
2020-04-06 19:56:22 -0400

    Add files via upload

--- voyage-Studios/Modular-Frame-Tween/ClientGUI_Tween.lua
+++ voyage-Studios/Modular-Frame-Tween/ClientGUI_Tween.lua
@@ -1,1 +1,29 @@
 
+local plr = game:GetService("Players").LocalPlayer
+local module = require(script.Parent.MouseHoverTween):Open(plr)
+local SP = script.Parent
+local debounce = false
+local co 
+
+game:GetService("RunService").RenderStepped:Connect(function()
+if debounce == false then
+	debounce = true
+co = coroutine.create(function()
+	for _,v in pairs(SP.mAINmENU:GetChildren()) do
+		if v:IsA("TextButton") then
+		v.MouseEnter:Connect(function()
+			local module = require(script.Parent.MouseHoverTween):Mouse(plr, v.Name, true)
+		end)
+
+		v.MouseLeave:Connect(function()
+			local module = require(script.Parent.MouseHoverTween):Mouse(plr, v.Name, false)
+			end)
+		end
+	end
+	coroutine.yield(co)
+end)
+coroutine.resume(co)
+wait(math.pi*2)
+debounce = false
+	end
+end)
